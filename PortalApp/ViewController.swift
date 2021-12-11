882bb518ec8affe61b455b00815180d1d028e6b4
mincheol <homerjay@naver.com>
2021-12-11 10:37:19 +0900

    Initial Commit

---
+++ PortalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PortalApp
+//
+//  Created by 申民鐡 on 2021/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
